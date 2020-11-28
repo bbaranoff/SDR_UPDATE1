fd08a74fe6a188f060e87b8b47ef0b0d4897199c
nirvana <bastienbaranoff@gmail.com>
2020-11-28 18:51:02 +0100

    commit

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 apt install libccid
+cd ~
+wget https://open-cells.com/opencells-mods-20190923.tgz
+tar xf opencells-mods-20190923.tgz
+sudo mysql -u root << END
+USE mysql;
+UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+FLUSH PRIVILEGES;
+END
+sudo systemctl restart mysql.service
+sudo mkdir -p /usr/local/etc/oai
+sudo cp -rp ~/opencells-mods/config_epc/* /usr/local/etc/oai
+cd openair-cn; source oaienv; cd scripts
+./check_hss_s6a_certificate /usr/local/etc/oai/freeDiameter hss.OpenAir5G.Alliance
+./check_mme_s6a_certificate /usr/local/etc/oai/freeDiameter mme.OpenAir5G.Alliance
+./root/opencells-mods/hss_import 127.0.0.1 root linux oai_db ~/opencells-mods/opencells_db.sql
 echo "Up to date !!"
 rm -rf ../SDR_UPDATE1
